fab39b87c2dd74e7ef3e58459d4b0ba6ad5bc54e
MUKILTEO\1725607 <1725607@mukilteo.wednet.edu>
2025-10-03 10:02:02 -0700

    Added camera movement for player.

---
+++ BlindSpot/Assets/Scripts/Player Camera.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    GameObject Player;
+    bool followPlayer = true;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (followPlayer == true)
+        {
+            camFollowPlayer();
+        }    
+    }
+
+    public void setFollowPlayer(bool val)
+    {
+        followPlayer = val;
+    }
+
+    void camFollowPlayer()
+    {
+        Vector3 newPos = new Vector3(Player.transform.position.x, Player.transform.position.y, this.transform.position.z);
+        this.transform.position = newPos;
+    }
+}
