0d8d2587685ca13f99ff180e71dfb2aeb929459d
MUKILTEO\1725607 <1725607@mukilteo.wednet.edu>
2025-10-03 09:47:10 -0700

    Added 1/3 Player movement.

---
+++ BlindSpot/Assets/Player.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public bool moving = false;
+    float speed = 5f; 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement(); 
+    }
+
+    void movement()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime, Space.World);
+            moving = true; 
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * speed * Time.deltaTime, Space.World);
+            moving = true;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+            moving = true;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime, Space.World);
+            moving = true;
+        }
+
+        if (Input.GetKey (KeyCode.D) != true && Input.GetKey (KeyCode.A) != true && Input.GetKey (KeyCode.S) != true && Input.GetKey (KeyCode.W) != true)
+        {
+            moving = false;
+        }
+    }
+}
