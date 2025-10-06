0791cf2fb04d0529c3ea46af7c6043a98b73ac51
MUKILTEO\1720941 <1720941>
2025-10-06 10:20:48 -0700

    more cool things

--- BlindSpot/Assets/People/Jacobe/Code/PressureSwitch.cs
+++ BlindSpot/Assets/People/Jacobe/Code/PressureSwitch.cs
@@ -1,24 +1,54 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
+using System;
 
 public class PressureSwitch : MonoBehaviour
 {
     [SerializeField] public GameObject currentDoor;
     public Animator anim;
-
+    public bool keepOpen;
     public UnityEvent OnOpen;
     public UnityEvent OnClose;
 
     public void OnTriggerEnter2D(Collider2D other)
     {
-        print("Trigger Entered");
-        currentDoor.GetComponent<Door>().AddPressureSwitch(this);
-        anim.SetBool("Open", true);
-        OnOpen.Invoke();
+        if (other.CompareTag("Bullet") && keepOpen)
+        {
+            print("Trigger Entered");
+            currentDoor.GetComponent<Door>().AddPressureSwitch(this);
+            anim.SetBool("Open", true);
+            OnOpen.Invoke();
+        }
+        else if (other.CompareTag("Box"))
+        {
+            print("Trigger Entered");
+            currentDoor.GetComponent<Door>().AddPressureSwitch(this);
+            anim.SetBool("Open", true);
+            OnOpen.Invoke();
+        }
+
     }
 
     public void OnTriggerExit2D(Collider2D other)
     {
+
+        if (keepOpen)
+        {
+            StartCoroutine(Delay());
+        }
+        else
+        {
+            print("Trigger Exited");
+            currentDoor.GetComponent<Door>().RemovePressureSwitch(this);
+            anim.SetBool("Open", false);
+            OnClose.Invoke();
+        }
+    }
+
+    public IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(5f);
         print("Trigger Exited");
         currentDoor.GetComponent<Door>().RemovePressureSwitch(this);
         anim.SetBool("Open", false);
