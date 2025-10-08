5bda50c5e9e22e3ac4234965bee72afcbde052ce
MUKILTEO\1720941 <1720941>
2025-10-08 09:53:08 -0700

    Prefab work

---
+++ BlindSpot/Assets/People/Jacobe/Code/FieldOfVeiw.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class FieldOfVeiw : MonoBehaviour
+{
+    private void Start()
+    {
+        Mesh mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        float fov = 90f;
+        Vector3 origin = Vector3.zero;
+        int rayCount = 2;
+        float angel = 0f;
+        float angelIncrease = fov / rayCount;
+        float viewDistance = 50f;
+
+        Vector3[] vertices = new Vector3[rayCount + 1 + 1];
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] triangles = new int[rayCount * 3];
+
+        vertices[0] = origin;
+
+        int vertexIndex = 1;
+        int triangleIndex = 0;
+        for (int i = 0; i < rayCount; i++)
+        {
+            Vector3 vertex = origin + vertices[i] * viewDistance;
+            vertices[vertexIndex] = vertex;
+
+            if (i > 0)
+            {
+                triangles[triangleIndex + 0] = 0;
+                triangles[triangleIndex + 1] = vertexIndex - 1;
+                triangles[triangleIndex + 2] = vertexIndex;
+
+                triangleIndex += 3;
+            }
+
+            vertexIndex++;
+            angel -= angelIncrease;
+        }
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+    }
+}
