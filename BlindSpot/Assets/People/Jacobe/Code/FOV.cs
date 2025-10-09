705d5148385853480ce8deec1805bc2dd81f73ac
MUKILTEO\1720941 <1720941>
2025-10-09 10:02:12 -0700

    Added camera/enemy detection

--- BlindSpot/Assets/People/Jacobe/Code/FOV.cs
+++ BlindSpot/Assets/People/Jacobe/Code/FOV.cs
@@ -1,31 +1,43 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FOV : MonoBehaviour
 {
-    public float fovAngle = 90f;
+    public float fovAngle = 5f;
     public Transform fovPoint;
     public float range = 8;
+    public LayerMask layer;
 
     public Transform target;
 
     private void Update()
     {
-        Vector2 dir = transform.position - transform.position;
-        float angel = Vector3.Angle(dir, fovPoint.up);
-        RaycastHit2D r = Physics2D.Raycast(fovPoint.position, dir, range);
+        Vector2 old = target.position - transform.position;
+        Vector2 dir = (new Vector2(old.y, -old.x)).normalized;
+        float angel = Vector3.Dot(dir, fovPoint.up);
+        RaycastHit2D r = Physics2D.Raycast(fovPoint.position, old.normalized, range, layer);
 
-        if (angel < fovAngle / 2)
+        //Debug.DrawRay(fovPoint.position, dir, Color.blue);
+
+        if (angel > fovAngle)
         {
-            if (r.collider.CompareTag("Player"))
+            if (r.collider && r.collider.CompareTag("Playera"))
             {
                 //Player Spotted
                 print("Spotted");
-                Debug.DrawRay(fovPoint.position, dir, Color.red);
+               
             }
             else
             {
                 print("Not there");
             }
+            
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 dir = (target.position - transform.position).normalized;
+        Gizmos.DrawLine(fovPoint.position, fovPoint.position + dir * range);
     }
 }
